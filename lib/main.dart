1c7b84798a1fcef844ecf26c77eba3529e039bac
yuki <yuki.laurentiu@gmail.com>
2023-04-25 11:21:08 -0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
