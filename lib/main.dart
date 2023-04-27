9b593c6c39063fbd10db90ade89e38577e31a2e9
yuki <yuki.laurentiu@gmail.com>
2023-04-26 18:22:20 -0600

    changed stateful widget and added setState function

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,17 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,9 +35,11 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
-                child: Image.asset('images/dice1.png'),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
                 onPressed: () {
-                  print('Hello, left');
+                  setState(() {
+                    leftDiceNumber = 5;
+                  });
                 },
               ),
             ),
@@ -36,10 +48,8 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
-                child: Image.asset('images/dice1.png'),
-                onPressed: () {
-                  print('Hello, right');
-                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {},
               ),
             ),
           ),
